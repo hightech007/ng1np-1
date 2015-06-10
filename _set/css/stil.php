566a4f815d29535094933db302033b9336810627
antistereotip <milutingavrilovic@yahoo.com>
2015-06-10 21:11:06 +0200

    Update stil.php

--- _set/css/stil.php
+++ _set/css/stil.php
@@ -1,4 +1,5 @@
 <?php
+//skupi stranice u niz, kompresuj, keshiraj i baferuj :)
 $css = array("master.css", "acc.css", "anm.css", "qrs.css");
 $bffr = "";
 foreach ($css as $cs) { $bffr .= file_get_contents($cs); }
@@ -10,7 +11,7 @@ header('Cache-Control: public');
 header('Expires: ' . gmdate('D, d M Y H:i:s', time() + 86400) . ' GMT');
 header("Content-type: text/css");
 
-//cache 
+//kesh
 $interval = 30 * 60;
 $filename = "../../cache/css/".basename(md5(rtrim($_SERVER["REQUEST_URI"],'/'))).".cache";
 if (file_exists($filename) && (time() - $interval) < filemtime($filename)) {
@@ -23,7 +24,7 @@ ob_start();
 echo($bffr);
 
 
-//buffer to cache file
+//baferuj u kesh
 $bffr = ob_get_contents(); 
 $file = fopen($filename,'w');
 fwrite($file, $bffr);
